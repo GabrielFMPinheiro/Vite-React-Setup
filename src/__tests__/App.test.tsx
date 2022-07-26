954b3f20f91f0ad64b7c1232240deda735f1dff5
Gabriel Pinheiro <g.felippe5965@gmail.com>
2022-07-26 16:32:32 -0300

    build: implement eslint

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,14 +1,14 @@
-import App from "../App";
-import { render, screen } from "@testing-library/react";
-import user from "@testing-library/user-event";
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import App from '../App';
 
-test("Renders main page correctly", async () => {
+test('Renders main page correctly', async () => {
   render(<App />);
-  const button = await screen.findByRole("button");
+  const button = await screen.findByRole('button');
   expect(button).toBeInTheDocument();
-  expect(button).toHaveTextContent("count is 0");
+  expect(button).toHaveTextContent('count is 0');
 
   user.click(button);
 
-  expect(button).toHaveTextContent("count is 1");
+  expect(button).toHaveTextContent('count is 1');
 });
